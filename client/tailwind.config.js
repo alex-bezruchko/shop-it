b15f1bc47a8e973793f98eef9c63472bee27627d
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-13 10:01:14 -0400

    initial commit, added tailwind and initial application header

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: '#0fa3b1',
+        secondaryBlue: '#b5e2fa',
+        primaryWhite: '#f9f7f3',
+        primaryBeige: '#eddea4',
+        primaryOrange: '#f7a072',
+      }
+    },
+  },
+  plugins: [],
+}
+
