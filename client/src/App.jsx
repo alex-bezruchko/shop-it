de1f636bb999a7246a75f9d4fd08c91188b7c842
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-14 15:42:45 -0400

    add account w/ subpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,10 +17,10 @@ function App() {
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/account" element={<AccountPage/>}/>
           <Route index element={<IndexPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
           <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/account/:subpage?" element={<AccountPage/>}/>
         </Route>
       </Routes>
     </UserContextProvider>
