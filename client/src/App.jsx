3a8d106d993bea06d56a149adcdca77f64f9a3b0
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-14 09:55:13 -0400

    add register, login, profile api with front-end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,29 @@
 
-import './App.css'
+import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Layout from './layouts/Layout.jsx';
+import axios from 'axios';
+import {UserContextProvider} from './components/UserContext.jsx';
+import AccountPage from './pages/AccountPage.jsx';
+import IndexPage from './pages/IndexPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+
+axios.defaults.baseURL = import.meta.env.VIVITE_SERVER_URL;
+axios.defaults.withCredentials = true;
 
 function App() {
-
   return (
-    <div>
-      <header className="flex p-4 justify-between">
-        <a href="" className="flex items-center gap-1">
-          <svg className="primaryBlue text-primaryBlue w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-            <path stroke="0fa3b1" fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
-          </svg>
-          <span className="font-bold text-xl">Shop-it</span>
-        </a>
-        <div className="flex gap-2 border border-blue-200 rounded-full p-2 px-3 shadow-md shadow-gray-300">
-          <div>Add products</div>
-          <button className="bg-primaryBlue text-white p-1 rounded-full">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-4">
-              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-            </svg>
-          </button>
-        </div>
-        <div className="flex items-center gap-2 border border-blue-200 rounded-full p-2 px-3 shadow-md shadow-gray-300">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <div className="bg-primaryBlue rounded-full text-white border border-primaryBlue overflow-hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
-          </svg>
-
-          </div>
-
-        </div>
-
-      </header>
-    </div>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/account" element={<AccountPage/>}/>
+          <Route index element={<IndexPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
