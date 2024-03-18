99fd82fe0417183303ea80b2b7853d60f855158b
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-18 11:07:56 -0400

    clean-up code

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -46,26 +46,9 @@ export default function ShoppingList() {
         }));
     }
     function addToList(product) {
-        console.log('addToList(product)', product)
-        // console.log('products', products)
-        // console.log('selectedProducts', selectedProducts)
 
-        // const existing = selectedProducts;
-        // if (selectedProducts.products.length !== 0) {
-        //     let ifDup = selectedProducts.products.find(p => p._id === product._id);
-        //     if (!ifDup) {
-        //         existing.products.push(product)
-        //         setSelectedProducts(existing)
-        //     }
-        // } else {
-        //     existing.products.push(product)
-        //     setSelectedProducts(existing)
-        //     console.log('after existing', selectedProducts)
-
-        // }
         const isDuplicate = selectedProducts.products.some(p => p._id === product._id);
 
-        // If it's not a duplicate, update the state with the new product added
         if (!isDuplicate) {
             setSelectedProducts(prevState => ({
                 ...prevState,
@@ -104,17 +87,9 @@ export default function ShoppingList() {
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-inset focus:ring-secondaryBlue sm:text-sm sm:leading-6"/>
                 </div>
             </div>
-            {selectedProducts.products.length < 1 && (
-                <></>
-                // <h2 className="nunito text-2xl pb-3 pt-5">
-
-                //     Nothing on the list yet. Find products to add:
-                //     {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-2">
-                //         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                //     </svg> */}
-                // </h2>
-            )
-            } <div className="flex flex-col text-center justify-center">
+            {selectedProducts.products.length < 1 && (<></>)} 
+            
+            <div className="flex flex-col text-center justify-center">
                 <label htmlFor="name" className="block nunito text-3xl pt-5">Products</label>
 
                     <div className="w-full">
