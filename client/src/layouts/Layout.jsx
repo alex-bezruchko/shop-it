dbc69c8efb9fcab9dc389c271a7f03379dc17b65
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-19 13:51:14 -0400

    add alert to layout

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,3 +1,4 @@
+import Alert from "../components/Alert.jsx";
 import Header from "./../components/Header.jsx";
 import {Outlet} from "react-router-dom";
 
@@ -5,6 +6,7 @@ export default function Layout() {
     return (
         <div className="p-6 sm:p-6 flex flex-col min-h-screen">
             <Header/>
+            <Alert/>
             <Outlet/>
         </div>
     )
