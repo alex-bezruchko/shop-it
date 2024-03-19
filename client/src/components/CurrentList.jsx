bbb8da6c90c2a8516277522f88bd28b4ae854bf6
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-19 13:58:11 -0400

    add product form to current list

--- client/src/components/CurrentList.jsx
+++ client/src/components/CurrentList.jsx
@@ -3,12 +3,10 @@ import { UserContext } from "./UserContext";
 import axios from 'axios';
 import { useParams } from "react-router-dom";
 import ProductList from "../components/ProductList";
+import ProductForm from "../components/ProductForm";
 
-
-// import { useDispatch, useSelector } from 'react-redux';
 
 export default function CurrentList() {
-    // const dispatch = useDispatch();
 
     const {user} = useContext(UserContext);
     const [ifNotEditing, setIfNotEditing] = useState(false);
@@ -44,19 +42,17 @@ export default function CurrentList() {
     async function updateShoppingList(updatedList) {
         try {
             const response = await axios.put(`${import.meta.env.VITE_SERVER_URL}/shoppinglists/${selectedListId}`, updatedList);
-            // Update local state with the updated list
-            setCurrentList(updatedList); // Trigger re-render by passing the updated list directly
+            if (response) {
+                setCurrentList(updatedList);
+            }
         } catch (error) {
             console.error('Error updating shopping list:', error);
         }
     }
     
     async function addToList(product) {
-        console.log('product', product);
-        console.log('currentList', currentList);
     
         const isDuplicate = currentList.products.some(p => p.product._id === product._id);
-        console.log('isDupe', isDuplicate);
     
         if (!isDuplicate) {
             const updatedProducts = [...currentList.products, { product, completed: false }];
@@ -67,20 +63,18 @@ export default function CurrentList() {
     
     async function checkItemFromList(itemId) {
         
-            const itemIndex = currentList.products.findIndex(item => item._id === itemId);
-        
-            if (itemIndex !== -1) { 
+        const itemIndex = currentList.products.findIndex(item => item._id === itemId);
     
-                currentList.products[itemIndex].completed = !currentList.products[itemIndex].completed;
-                updateShoppingList(updatedList);
-    
-                
-            } else {
-                console.log('Product not found in the list.');
-            }
+        if (itemIndex !== -1) { 
+
+            currentList.products[itemIndex].completed = !currentList.products[itemIndex].completed;
+            updateShoppingList(updatedList);
+            
+        } else {
+            console.log('Product not found in the list.');
+        }
     }
     
-
     async function checkItemFromList(itemId) {
  
         const itemIndex = currentList.products.findIndex(item => item._id === itemId);
@@ -91,8 +85,9 @@ export default function CurrentList() {
 
             try {
                 const response = await axios.put(`${import.meta.env.VITE_SERVER_URL}/shoppinglists/${selectedListId}`, { name: currentList.name, products: currentList.products });
-                // Update local state with the updated list
-                setCurrentList({ ...currentList }); // Trigger re-render by creating a new object reference
+                if (response) {
+                    setCurrentList({ ...currentList });
+                }
             } catch (error) {
                 console.error('Error updating shopping list:', error);
             }
@@ -113,21 +108,7 @@ export default function CurrentList() {
             }
         }
     }
-    // async function addToList(product) {
-    //     console.log('product', product)
-    //     console.log('currentList', currentList)
 
-    //     const isDuplicate = currentList.products.some(p => p.product._id === product._id);
-    //     console.log('isDupe', isDuplicate);
-
-    //     if (!isDuplicate) {
-    //         const updatedProducts = [...currentList.products, { product, completed: false }];
-    //         setCurrentList(prevState => ({
-    //             ...prevState,
-    //             products: updatedProducts
-    //         }));
-    //     }
-    // }
     async function searchProducts(e) {
         e.preventDefault();
         let body = e.target.value;
@@ -159,6 +140,27 @@ export default function CurrentList() {
         } 
     }
 
+    async function deleteProduct(product) {
+        console.log('current list deleteProduc(product)', product)
+    }
+
+    async function updateProduct(formData) {
+        let product = formData.formData;
+        product._id = '';
+        product._id = formData._id;
+        let newList = [...currentList.products];
+        const productIndex = newList.findIndex(item => item.product._id === product._id);
+
+        newList[productIndex] = {
+            ...newList[productIndex],
+            product: {
+                ...newList[productIndex].product,
+                ...product
+            }
+        };
+    
+        setCurrentList({...currentList, products: newList});
+    }
     
     async function deleteProductFromList(id) {
         console.log(id)
@@ -220,13 +222,20 @@ export default function CurrentList() {
                             <div className={`flex items-center w-full justify-between bg-white rounded-lg shadow-md p-3 mb-4 border-2 ${product.completed ? 'border-primaryBlue' : 'border-primaryOrange'}`}>
                                 <div className="flex items-center w-full justify-between">
                                     <div className="mr-4 flex flex-col w-full justify-between">
-                                    
                                         <h3 className="text-left text-lg font-medium lora self-start">{product.product.name}</h3>
                                         <div className="pt-3">
                                             <p className="text-left text-sm nunito">{product.product.description}</p>
                                             <p className="text-left text-sm nunito">${product.product.price}</p>
                                         </div>
                                     </div>
+                                    <div className="flex">
+                                        <ProductForm 
+                                            className="self-center" 
+                                            product={product.product} 
+                                            handleDeleteProduct={deleteProduct} 
+                                            updateProduct={updateProduct}
+                                        />
+                                        </div>
                                     <img src={product.product.photo} alt="Product Photo" className="w-24 h-auto pr-4"/>
                                 </div>
                                 <div>
@@ -241,9 +250,9 @@ export default function CurrentList() {
                                     )}
                                 </div>
                             </div>
-                            <button onClick={() => deleteProductFromShoppingList(product.product._id)} className="primaryRed ml-4 mb-4 p-1 self-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            <button onClick={() => deleteProductFromShoppingList(product.product._id)} className="text-primaryRed ml-4 mb-4 p-1 self-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
                                 </svg>
                             </button>
                         </div>
