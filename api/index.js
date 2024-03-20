7db09daedc821db9ea5aa326c838cae6855857db
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-19 21:20:40 -0400

    unrestrict origins in cors

--- api/index.js
+++ api/index.js
@@ -18,7 +18,11 @@ const jwtSecret = 'pass123';
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}));
 
 mongoose.connect(process.env.MONGO_URL);
 app.options('*', cors()); // Handle preflight requests for all routes
