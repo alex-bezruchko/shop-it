af1bea9f0a58b7c4a2008fda81ef7296f4cbb693
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-19 20:07:19 -0400

    add style to alerts

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -14,7 +14,8 @@ const AlertContainer = ({ alert, removeAlert }) => {
     <div className='flex justify-center'>
       {message && alertType && (
         
-        <div className={`w-full text-center md:w-2/3 lg:w-2/3 xl:w-2/3 flex justify-center sm:justify-between  mt-8 p-3 rounded border border-1 border-${alertType}`}>
+        <div className={`w-full text-center md:w-2/3 lg:w-2/3 xl:w-2/3 flex justify-center sm:justify-between mt-8 p-3 rounded border border-1 border-${alertType} ${alertType === 'primaryGreen' ? 'bg-lightGreen' : 'bg-lightRed'}` }>
+
           <span>{message}</span>
           <button onClick={handleClose}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`w-6 h-6 text-${alertType}`}>
