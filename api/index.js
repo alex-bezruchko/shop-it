8b594cbe5da5daea6eba2af6fb9c5f4ee4dea73c
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-20 09:33:25 -0400

    add credentials prop back to cors

--- api/index.js
+++ api/index.js
@@ -21,7 +21,8 @@ app.use(cookieParser());
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true
 }));
 
 mongoose.connect(process.env.MONGO_URL);
