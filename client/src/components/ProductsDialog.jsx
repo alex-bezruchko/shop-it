e646f6d521bea04a31f51ec878de1f3a36db4132
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-03-27 13:11:29 -0400

    fix event default error in clear photo

--- client/src/components/ProductsDialog.jsx
+++ client/src/components/ProductsDialog.jsx
@@ -64,7 +64,10 @@ import {
     function handleImageSelect(selectedPhoto) {
         setPhoto(selectedPhoto);
     }
-
+    
+    function removePhoto() {
+        setPhoto('');
+    }
     return (
         <>
             <div>Add products</div>
@@ -123,7 +126,7 @@ import {
                                     
                                         <div className="flex items-center border-r w-full">
                                             <img src={photo} alt="Selected" className="w-40 h-40" />
-                                            <button onClick={() => removePhoto()} className="text-primaryRed mt-2 mx-auto text-center">
+                                            <button onClick={removePhoto} className="text-primaryRed mt-2 mx-auto text-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 ml-1">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
                                                 </svg>
