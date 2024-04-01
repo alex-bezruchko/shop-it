f06c9ec1a48ee9d0ba5487099905d8d37a34974a
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-04-01 13:11:07 -0400

    modify user state props in userContext

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -4,18 +4,20 @@ import axios from 'axios';
 export const UserContext = createContext({});
 
 export function UserContextProvider({children}) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState({email: '', name: '', _id: ''});
     const [ready, setReady] = useState(false);
     useEffect(() => {
-        if(!user) {
+        if(!user._id) {
             axios.get(`/users/profile`, {withCredentials: true}).then(({data}) => {
                 setUser(data);
                 setReady(true);
+                console.log(user)
             }).catch(error => {
                 console.log(error)
             });
         }
     }, []);
+    console.log(user)
     return (
         <UserContext.Provider value={{user, setUser, ready}}>
             {children}
