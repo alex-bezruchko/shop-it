2c4ec2a61f9801cb4a9ddfd2d54f2c21e4c6708e
alex.bezruchko@gmail.com <alex.bezruchko@gmail.com>
2024-04-04 11:43:40 -0400

    add remove alert after time

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,10 +1,20 @@
 // AlertContainer.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { removeAlert } from './../actions/alertActions';
 
 const AlertContainer = ({ alert, removeAlert }) => {
   const { message, alertType } = alert;
+
+  useEffect(() => {
+    if (message && alertType) {
+      const timer = setTimeout(() => {
+        removeAlert();
+      }, 3800); // 3 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [message, alertType, removeAlert]);
 
   const handleClose = () => {
     removeAlert();
@@ -13,15 +23,12 @@ const AlertContainer = ({ alert, removeAlert }) => {
   return (
     <div className='flex justify-center'>
       {message && alertType && (
-        
-        <div className={`w-full text-center md:w-2/3 lg:w-2/3 xl:w-2/3 flex justify-center sm:justify-between mt-8 p-3 rounded border border-1 border-${alertType} ${alertType === 'primaryGreen' ? 'bg-lightGreen' : alertType === 'primaryOrange' ? 'bg-lightOrange' : 'bg-lightRed'}` }>
-
+        <div className={`w-full text-center md:w-2/3 lg:w-2/3 xl:w-2/3 flex justify-between sm:justify-between mt-8 p-3 rounded border border-1 border-${alertType} ${alertType === 'primaryGreen' ? 'bg-lightGreen' : alertType === 'primaryOrange' ? 'bg-lightOrange' : 'bg-lightRed'}` }>
           <span>{message}</span>
           <button onClick={handleClose}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`w-6 h-6 text-${alertType}`}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`w-6 h-6 text-${alertType}`}>
               <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-          </svg>
-
+            </svg>
           </button>
         </div>
       )}
