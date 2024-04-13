9e247aea53ec217bc8737419a3de46f96ef8a80e
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-13 12:21:11 -0400

    refactor favorite styles across the app

--- client/src/pages/FriendDetailsPage.jsx
+++ client/src/pages/FriendDetailsPage.jsx
@@ -218,7 +218,7 @@ export default function FriendDetailPage({listLoading}) {
                       <p className="text-md nunito text-left">{place.address}</p>
                   </div>
 
-                  <svg onClick={() => togglePlace(place)} xmlns="http://www.w3.org/2000/svg"  fill={isPlaceFavorite(place) ? '#0fa3b1' : 'none'}  viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="min-w-[40px] h-[40px] text-primaryBlue">
+                  <svg onClick={() => togglePlace(place)} xmlns="http://www.w3.org/2000/svg"  fill={isPlaceFavorite(place) ? 'rgb(247, 160, 114)' : 'none'}  viewBox="0 0 24 24" strokeWidth="1.5" stroke={isPlaceFavorite(place) ? 'none' : 'rgb(247, 160, 114)'} className="min-w-[40px] h-[40px] text-primaryBlue">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                   </svg>
                       
