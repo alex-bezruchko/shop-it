cff572476314ae4cf619146dc2e1190c38291db0
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-18 08:51:30 -0400

    add footer to layout

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,13 +1,15 @@
 import Alert from "../components/Alert.jsx";
 import Header from "./../components/Header.jsx";
+import Footer from "./../components/Footer.jsx";
 import {Outlet} from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div className="p-3 md:p-6 flex flex-col min-h-screen">
+        <div className="p-3 md:p-6 flex flex-col min-h-screen h-screen">
             <Header/>
             <Alert/>
             <Outlet/>
+            <Footer/>     
         </div>
     )
 }
