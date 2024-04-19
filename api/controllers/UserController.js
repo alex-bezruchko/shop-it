8c8a7565bd61620b4e7b4edb2767c08a18b74d1c
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-19 12:27:23 -0400

    change backed to send the user a message when they sent a request

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -417,7 +417,17 @@ exports.send = async (req, res) => {
             _id: sender._id
         };
 
-        res.status(200).json({ friendRequests: [friendRequest], outgoingRequests: [] });
+        const outgoingRequest = {
+            receiver: {
+                _id: receiver._id,
+                name: receiver.name,
+                email: receiver.email
+            },
+            status: newRequest.status,
+            _id: receiver._id
+        };
+
+        res.status(200).json(outgoingRequest);
 
         // Send Pusher notification to receiver
         sendNotification(`user-${receiverId}`, 'friend-request', { 
@@ -430,6 +440,21 @@ exports.send = async (req, res) => {
                     email: sender.email
                 },
                 receiver: receiverId,
+                status: 'pending'
+            }
+        });
+        // Send Pusher notification to sender
+
+        sendNotification(`user-${sender._id}`, 'friend-request-submitted', { 
+            message: 'You submitted a friend request.', 
+            outgoingRequest: {
+                _id: receiver._id,
+                receiver: {
+                    _id: receiver._id,
+                    name: receiver.name,
+                    email: receiver.email
+                },
+                sender: sender._id,
                 status: 'pending'
             }
         });
