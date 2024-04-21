9a40589d3d9817df3d45866e6ad22ffbae4013ab
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-21 18:14:31 -0400

    adjust fonts, margin on navs

--- client/src/components/ProductsDialog.jsx
+++ client/src/components/ProductsDialog.jsx
@@ -7,14 +7,6 @@ import FileUpload from './FileUpload';
 import { Validation } from './Validation';
 import ValidationErrorDisplay from "./../components/ValidationErrors";
 
-import {
-    Button,
-    Dialog,
-    DialogBody,
-    DialogFooter,
-  } from "@material-tailwind/react";
-   
-
   export default function ProductsDialog({product, handleUpdateProducts}) {
     
     const dispatch = useDispatch();
@@ -32,7 +24,7 @@ import {
     const [formType, setFormType] = useState('create')
 
     const validationDivRef = useRef(null);
-    // const handleOpen = () => setOpen(!open);
+
     const handleOpen = () => {
         if (!open) {
             if (product) {
@@ -141,8 +133,6 @@ import {
             body.photo = photo;
             try {
                 const response = await axios.put(`/products/${id}`, body);
-                // Update local state with the updated name
-                // updateProduct(newItem);
                 dispatch({ type: 'SET_ALERT', payload: {message: 'Product updated successfully', alertType: 'primaryGreen'} });
                 setLoading(false);
                 body.id = id;
@@ -183,7 +173,7 @@ import {
         <>
             {formType === 'create' && (
                 <button onClick={handleOpen} className="bg-primaryBlue text-white p-1 rounded-full px-3 flex h-full items-center justify-between">
-                    <p className="nunito text-lg pr-2">Add</p>
+                    <p className="nunito text-md pr-2 self-end">Add</p>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                     </svg>
