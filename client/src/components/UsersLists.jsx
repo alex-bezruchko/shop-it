6cad8b7a110889927a41499aab48bdd1ef532d06
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-22 10:26:29 -0400

    attempt to fix performance for userlist and accountPage

--- client/src/components/UsersLists.jsx
+++ client/src/components/UsersLists.jsx
@@ -79,13 +79,13 @@ export default function UsersLists({sendTo, currentLink, listLoading, isLoading}
                     {currentLists.length !== 0 ? (
                         <div className="w-full">
                             <div className="tabs flex justify-around">
-                                <button aria-label="Current Tab button" onClick={() => handleTab('current')} className={activeTab === 'current' ? 'text-center nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists bg-primaryOrange flex justify-around items-center text-white' : 'text-center text-black nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists flex justify-around items-center'}>
+                                <button aria-label="Current Tab button" onClick={handleTab('current')} className={activeTab === 'current' ? 'text-center nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists bg-primaryOrange flex justify-around items-center text-white' : 'text-center text-black nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists flex justify-around items-center'}>
                                     <span>Current</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={activeTab === 'current' ? ' w-[35px] h-[35px] text-white' : ' w-[35px] h-[35px] text-black'}>
                                         <path stroke={activeTab === 'current' ? 'currentColor' : '#FF6E4B'} strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
                                     </svg>
                                 </button>
-                                <button aria-label="Completed Tab button" onClick={() => handleTab('completed')} className={activeTab === 'completed' ? 'text-center nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists bg-primaryGreen flex justify-around items-center text-white' : 'text-center text-black nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists flex justify-around items-center'}>
+                                <button aria-label="Completed Tab button" onClick={handleTab('completed')} className={activeTab === 'completed' ? 'text-center nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists bg-primaryGreen flex justify-around items-center text-white' : 'text-center text-black nunito text-xl p-2 w-1/2 rounded sm:text-3xl lists flex justify-around items-center'}>
                                     <span>Completed</span>
                                     {activeTab === 'completed' ? (
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[35px] h-[35px]">
@@ -100,36 +100,37 @@ export default function UsersLists({sendTo, currentLink, listLoading, isLoading}
 
                                 </button>
                             </div>
-
-                            <ul className="mt-8">
-                                {filteredList.map(list => (
-                                    <li key={list._id} className="flex w-full justify-between">
-                                        <div onClick={() => viewList(list._id)} className={`flex w-full items-center justify-between bg-white rounded-lg shadow-lg p-4 mb-4 border border-[1.5px] ${list.completed ? 'border-primaryGreen' : 'border-primaryOrange'}`}>
-                                            <div className="flex w-full items-center w-full justify-between">
-                                                <div className="mr-4">
-                                                    <h2 className="text-left text-xl lora font-medium">{list.name}</h2>
+                            
+                            {filteredList.length === 0 ? (
+                                <h2 className="flex text-center text-xl nunito justify-center mt-2">
+                                    No lists found. Create one here: 
+                                    
+                                    <Link to={'/account/new'}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="#6CB462" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-full ml-2 text-white">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" className="h-full"/>
+                                        </svg>
+                                    </Link>
+                                </h2>
+                            ) : (
+                                <ul className="mt-8">
+                                    {filteredList.map(list => (
+                                        <li key={list._id} className="flex w-full justify-between">
+                                            <div onClick={() => viewList(list._id)} className={`flex w-full items-center justify-between bg-white rounded-lg shadow-lg p-4 mb-4 border border-[1.5px] ${list.completed ? 'border-primaryGreen' : 'border-primaryOrange'}`}>
+                                                <div className="flex w-full items-center w-full justify-between">
+                                                    <div className="mr-4">
+                                                        <h2 className="text-left text-xl lora font-medium">{list.name}</h2>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <button aria-label="Delete List button" onClick={() => deleteList(list._id)} className="text-primaryRed ml-4 mb-4 p-1 self-center">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
-                                            </svg>
-                                        </button>
-                                    </li>
-                                ))}
-                                {filteredList.length === 0 && (
-                                    <h2 className="flex text-center text-xl nunito justify-center mt-2">
-                                        No lists found. Create one here: 
-                                        
-                                        <Link to={'/account/new'}>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="#6CB462" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-full ml-2 text-white">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" className="h-full"/>
-                                            </svg>
-                                        </Link>
-                                    </h2>
-                                )}
-                            </ul>
+                                            <button aria-label="Delete List button" onClick={() => deleteList(list._id)} className="text-primaryRed ml-4 mb-4 p-1 self-center">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
+                                                </svg>
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
                         </div>
                     ) : (
                         <div className="flex flex-col text-center justify-center">
