e1dcb5edbc1d00e60a37ce670e472cce0c7d40fe
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-22 01:55:11 -0400

    re-factor code to fetch only one at a time

--- client/src/components/CurrentList.jsx
+++ client/src/components/CurrentList.jsx
@@ -297,7 +297,7 @@ export default function CurrentList({listLoading, isLoading}) {
 
                                     <ul className="flex flex-col justify-between">
                                         {currentList.products?.map(product => (
-                                            <div key={product.product._id} className="flex w-full justify-between">
+                                            <li key={product.product._id} className="flex w-full justify-between">
                                                 <div className={`flex items-center w-full justify-between bg-white rounded-lg shadow-lg p-0 mb-4 border border-2 ${updateLoading && product._id === idLoaded ? 'border-primaryBlue' : (product.completed ? 'border-primaryGreen' : 'border-primaryOrange')}`}>
 
                                                 {/* <div className={`flex items-center w-full justify-between bg-white rounded-lg shadow-lg p-0 mb-4 border border-2 ${product.completed ? 'border-primaryBlue' : 'border-primaryOrange'}`}> */}
@@ -367,7 +367,7 @@ export default function CurrentList({listLoading, isLoading}) {
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
                                                     </svg>
                                                 </button>
-                                            </div>
+                                            </li>
                                         ))}
                                     </ul>
                                 </div>
