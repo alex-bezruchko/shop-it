780beccd9d64278254c56828a6a212129cd4c396
Alex Bezruchko <alex.bezruchko@gmail.com>
2024-04-25 11:06:06 -0400

    fix infinite re-render

--- client/src/components/UserContext.jsx
+++ client/src/components/UserContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 export const UserContext = createContext({});
@@ -6,26 +7,26 @@ export const UserContext = createContext({});
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState({ email: '', name: '', _id: '' });
     const [ready, setReady] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data } = await axios.get(`/users/profile`, { withCredentials: true });
+                setUser(data);
+                setReady(true);
+            } catch (error) {
+                setUser({ email: '', name: '', _id: '' });
+                setReady(true);
+                navigate(`/login`);
+            }
+        };
+
         if (!ready) {
-            axios.get(`/users/profile`, { withCredentials: true })
-                .then(({ data }) => {
-                    setUser(data);
-                    setReady(true);
-                })
-                .catch(error => {
-                    setUser({ email: '', name: '', _id: '' });
-                    setReady(true);
-                    console.log(error);
-                });
-        } else {
-            setReady(true);
+            fetchData();
         }
-    }, [ready]);
-
+    }, [ready, navigate]);
     
-
     return (
         <UserContext.Provider value={{ user, setUser, ready }}>
             {children}
